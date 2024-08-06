a07fa92b5aafd66cbc4b694bb2f37cd195849122
SmallTing1124 <chentt1124@gmail.com>
2024-08-06 14:42:09 +0800

    Restructure project and fix folder hierarchy

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+const num = ref(2);
+function clickMe() {
+    num.value++
+}
+</script>
+
+<template>
+    <div>
+        {{ num }} <!-- 會使用2個大括號來定義值 -->
+        <button type="button" v-on:click="clickMe">點我</button>
+    </div>
+</template>
